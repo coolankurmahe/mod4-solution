8a305032d1de70c4c3db94c5a4cc33e44f547186
ankurmahe <coolankurmahe@gmail.com>
2017-02-10 01:46:51 +0530

    URL secured

--- src/data/menudata.service.js
+++ src/data/menudata.service.js
@@ -1,47 +1,3 @@
-// (function () {
-// 'use strict';
-//
-// angular.module('Data')
-// .service('MenuDataService', MenuDataService);
-// // .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
-//
-//
-// MenuDataService.$inject = ['$http'];
-// function MenuCategoriesService($http)
-//  {
-//   var service = this;
-//   var ApiBasePath= "http://davids-restaurant.herokuapp.com";
-//   service.getAllCategories = function ()
-//    {
-//     var response = $http({
-//       method: "GET",
-//       url: (ApiBasePath + "/categories.json")
-//     });
-//
-//     return response;
-//   };
-//
-//
-//   service.getItemsForCategory = function (categoryShortName)
-//    {
-//     var response = $http({
-//       method: "GET",
-//       url: (ApiBasePath + "/menu_items.json"),
-//       params: {
-//         category: categoryShortName
-//       }
-//     });
-//
-//     return response;
-//   };
-//
-// }
-//
-//
-// })();
-
-
-
 
 (function () {
 'use strict';
@@ -53,7 +9,7 @@ MenuDataService.$inject = ['$http'];
 function MenuDataService($http)
  {
   var service = this;
-  var ApiBasePath= "http://davids-restaurant.herokuapp.com";
+  var ApiBasePath= "https://davids-restaurant.herokuapp.com";
   service.getAllCategories = function ()
    {
     var response = $http({
